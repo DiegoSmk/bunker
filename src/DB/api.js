7d58d6d41627354e3435c327b36ae6bb6594df6d
Diego Pena <diego.nib@outlook.com>
2021-01-04 15:37:44 -0300

    fake api

---
+++ src/DB/api.js
@@ -0,0 +1,64 @@
+const apiText = [
+  {
+    id: 1,
+    name: 'Alex',
+    resume: {
+      wins: 0,
+      loses: 0
+    },
+    coin: 1,
+    pow: 3,
+    def: 3,
+    int: 3
+  },
+  {
+    id: 2,
+    name: 'Claudio',
+    resume: {
+      wins: 0,
+      loses: 0
+    },
+    coin: 1,
+    pow: 2,
+    def: 5,
+    int: 2
+  },
+  {
+    id: 3,
+    name: 'Bruna',
+    resume: {
+      wins: 0,
+      loses: 0
+    },
+    coin: 1,
+    pow: 7,
+    def: 1,
+    int: 1
+  },
+  {
+  id: 4,
+  name: 'Smolker',
+  resume: {
+    wins: 0,
+    loses: 0
+  },
+  coin: 1,
+  pow: 6,
+  def: 1,
+  int: 2
+  },
+  {
+    id: 5,
+    name: 'Romeu',
+    resume: {
+      wins: 0,
+      loses: 0
+    },
+    coin: 1,
+    pow: 5,
+    def: 3,
+    int: 1
+    }
+]
+
+export default apiText
